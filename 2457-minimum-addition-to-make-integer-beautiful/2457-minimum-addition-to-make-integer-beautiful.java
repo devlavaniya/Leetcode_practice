7c43cd4ff226b1d2c1b0ea4632f53603eec43591
Dev Lavaniya <devlavaniya18@gmail.com>
2026-07-14 10:11:51 +0530

    Time: 0 ms (100%), Space: 42.1 MB (68.75%) - LeetHub

--- 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.java
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.java
@@ -20,3 +20,5 @@ class Solution {
         return n - original;
     }
 }
+
+// class Solution { public int digitsum(long n){ int sum =0; while(n>0){ sum+= (int)n%10; n=n/10; } return sum; } public long makeIntegerBeautiful(long n, int target) { if(digitsum(n)<=target) return 0; long org = n; long base = 10; while(digitsum(n)>target){ n = (n/base + 1) * base; base*=10; } return n-org; } }
