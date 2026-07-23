14dff366e7912ef74e17e849e9e6f62819f4ffa5
Dev Lavaniya <devlavaniya18@gmail.com>
2026-07-23 11:29:40 +0530

    Time: 0 ms (100%), Space: 41.8 MB (92.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,26 @@
 class Solution {
-    public int ways(int i , int j , int m , int n , int[][] dp){
-        if(i>=m || j>=n) return 0;
-        if(i==m-1 && j==n-1) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j] = ways(i+1 , j , m ,n , dp) + ways(i,j+1,m,n,dp);
+    // public int ways(int i , int j , int m , int n , int[][] dp){
+    //     if(i>=m || j>=n) return 0;
+    //     if(i==m-1 || j==n-1) return 1;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     return dp[i][j] = ways(i+1 , j , m ,n , dp) + ways(i,j+1,m,n,dp);
 
-    }
+    // }
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+        // int[][] dp = new int[m][n];
+        // for(int i=0 ; i<m ; i++){
+        //     for(int j=0 ; j<n ; j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
+        // return ways(0,0,m,n,dp);
+         int[][] dp = new int[m][n];
         for(int i=0 ; i<m ; i++){
             for(int j=0 ; j<n ; j++){
-                dp[i][j]=-1;
+                if(i==0 || j==0) dp[i][j]=1;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
-        return ways(0,0,m,n,dp);
+        return dp[m-1][n-1];
     }
 }
