b07f89c36d3d070d56128e64c0d72f8b9060b014
Dev Lavaniya <devlavaniya18@gmail.com>
2026-07-27 11:34:29 +0530

    Time: 26 ms (78.75%), Space: 44.3 MB (86.36%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,19 +1,29 @@
 class Solution {
-    public int sq(int n , int[] dp){
-        int a = (int) Math.sqrt(n);
-        if(a*a == n) return 1;
-        if(dp[n]!=-1) return dp[n];
-        int min = Integer.MAX_VALUE;
-        for(int i=1 ; i*i<=n ; i++){
-            int c = sq(i*i , dp) +  sq(n-i*i , dp);
-            min = Math.min(min ,c);
-        }
-        return dp[n] = min;
-    }
+    // public int sq(int n , int[] dp){
+    //     int a = (int) Math.sqrt(n);
+    //     if(a*a == n) return 1;
+    //     if(dp[n]!=-1) return dp[n];
+    //     int min = Integer.MAX_VALUE;
+    //     for(int i=1 ; i*i<=n ; i++){
+    //         int c = sq(i*i , dp) +  sq(n-i*i , dp);
+    //         min = Math.min(min ,c);
+    //     }
+    //     return dp[n] = min;
+    // }
     public int numSquares(int n) {
         int []dp = new int [n+1];
-        Arrays.fill(dp , -1);
-        return sq(n , dp);
-        
+       for(int i=1 ; i<=n ; i++){
+         int a = (int) Math.sqrt(i);
+         if(a*a == i) dp[i]= 1;
+         else{
+            int min = Integer.MAX_VALUE;
+            for(int j=1 ; j*j<=i ; j++ ){
+                min = Math.min( min , dp[j*j] + dp[i-j*j]);
+            }
+            dp[i]=min;
+
+         }
+       }
+        return dp[n];
     }
 }
