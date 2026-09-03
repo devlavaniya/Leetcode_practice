35dd78d148dd78755ac5035b3e9585abfe2e1e63
Dev Lavaniya <devlavaniya18@gmail.com>
2026-09-03 20:20:35 +0530

    Time: 8 ms (50%), Space: 121.7 MB (62.32%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,54 +1,31 @@
 class Solution {
 
-    static boolean yes(int[] arr) {
-        int even = 0, odd = 0;
-
-        for (int x : arr) {
-            if (x % 2 == 0)
-                even++;
-            else
-                odd++;
-        }
-
-        return even == arr.length || odd == arr.length;
-    }
-
     public boolean uniformArray(int[] arr) {
 
-        int n = arr.length;
+        int minOdd = Integer.MAX_VALUE;
 
-        if (yes(arr))
+        // smallest odd find karo
+        for (int x : arr) {
+            if (x % 2 != 0) {
+                minOdd = Math.min(minOdd, x);
+            }
+        }
+
+        // Agar koi odd hi nahi hai,
+        // array already all even hai
+        if (minOdd == Integer.MAX_VALUE)
             return true;
 
-        int j = -1;
+        // Har even ko smallest odd se subtract
+        for (int x : arr) {
 
-        // smallest odd element ka index
-        for (int i = 0; i < n; i++) {
-            if (arr[i] % 2 != 0) {
-                if (j == -1 || arr[i] < arr[j]) {
-                    j = i;
-                }
-            }
-        }
+            if (x % 2 == 0) {
 
-        int[] nums = new int[n];
-
-        for (int i = 0; i < n; i++) {
-
-            if (arr[i] % 2 != 0) {
-                nums[i] = arr[i];
-            } 
-            else {
-
-            
-                if (i == j || arr[i] - arr[j] < 1) {
+                if (x - minOdd < 1)
                     return false;
-                }
-
-                nums[i] = arr[i] - arr[j];
             }
         }
 
-        return yes(nums);
+        return true;
     }
 }
